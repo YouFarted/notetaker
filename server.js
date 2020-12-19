4a8aba78395a450edaa23acfeb0f8f1cf8cdc832
Andrew Olson <frrrkin@gmail.com>
2020-12-18 22:39:16 -0800

    resolved a middleware problem.  Express wants a seperate body-parser now

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
-const express = require('express')
-const path = require('path')
-const fs = require('fs')
-const loadDatabase = require('./lib/dbManagement').loadDatabase
+const fs =           require('fs')
+const path =         require('path')
+const express =      require('express')
+const bodyParser =   require('body-parser');
+const dbManagement = require('./lib/dbManagement')
 
 const app = express()
 const PORT = process.env.port | 8080
 
+// middleware
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.get('/api/notes', function(req, res) { loadDatabase(res) });
+app.get('/api/notes', dbManagement.loadDatabase );
 app.get('/notes', function(req, res) {
     res.sendFile(path.join(__dirname + '/public/notes.html'));
 });
@@ -16,5 +19,7 @@ app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
+app.post('/api/notes', dbManagement.addToDatabase )
 
-app.listen(PORT, () => console.log(`Example app listening at http://localhost:${PORT}`))
+
+app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`))
