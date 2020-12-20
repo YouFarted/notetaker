9fcd743c212873c01471ec70bfe16f2b2f51136f
Andrew Olson <frrrkin@gmail.com>
2020-12-20 14:00:43 -0800

    added a test for internalLoadDbFile

--- tests/dbManagement.test.js
+++ tests/dbManagement.test.js
@@ -5,6 +5,7 @@ jest.mock('fs')
 
 test('verify internalSaveDbFile saves to a file', () => {
 
+    const saveThis = { a: 1, b: 2 };
     const expectedToSave = '{"a":1,"b":2}'
     let whatWasSaved = null
 
@@ -18,9 +19,35 @@ test('verify internalSaveDbFile saves to a file', () => {
         });
 
     // Act
-    dbManagement.internalSaveDbFile({ a: 1, b: 2 }, function () { })
+    dbManagement.internalSaveDbFile(saveThis, function () { })
 
     // Assert
     expect(whatWasSaved).toBe(expectedToSave)
     expect(spyWriteFile).toHaveBeenCalled()
+    expect(spyAccess).toHaveBeenCalled()
+});
+
+test('verify internalLoadDbFile loads from a file', () => {
+
+    const expectedToLoad = { a: 1, b: 2 };
+    const pretendToLoadThisData = '{"a":1,"b":2}'
+    let whatWasLoaded = null
+    // Arrange
+
+    const spyAccess = jest.spyOn(fs, "access")
+        .mockImplementation((_dbdir, cb) => cb(null));
+    const spyReadFile = jest.spyOn(fs, 'readFile')
+        .mockImplementation((_1, cb) => {
+            cb(null, pretendToLoadThisData)
+        });
+
+    // Act
+    dbManagement.internalLoadDbFile(function (err, data) {
+        whatWasLoaded = data
+    })
+
+    // Assert
+    expect(whatWasLoaded).toEqual(expectedToLoad)
+    expect(spyReadFile).toHaveBeenCalled()
+    expect(spyAccess).toHaveBeenCalled()
 });
